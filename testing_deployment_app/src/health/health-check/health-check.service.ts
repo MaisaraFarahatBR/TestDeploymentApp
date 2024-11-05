953b5917ffc68d03de1d34568b9ba523962d734a
MaisaraFarahatBR <mfa@blueribbon-inc.com>
2024-11-05 17:01:15 +0200

    adding Ezz

--- testing_deployment_app/src/health/health-check/health-check.service.ts
+++ testing_deployment_app/src/health/health-check/health-check.service.ts
@@ -5,6 +5,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthCheckService {
     checkHealth(): { status: string } {
-        return { status: 'OK fineeeee w ben7ebbak ya madooooooooo' }; // Return a simple health status
+        return { status: 'OK fineeeee w ben7ebbak ya madooooooooo w ya EZZZZZ' }; // Return a simple health status
     }
 }
