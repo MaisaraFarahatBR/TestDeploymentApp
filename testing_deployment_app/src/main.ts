4f99b681716fe0d6ac4da28f8ec2f0c3a8303b81
MaisaraFarahatBR <mfa@blueribbon-inc.com>
2024-11-05 14:23:20 +0200

    updating port
    port 3000 is used by ubuntu machine i think so i will try 4000

--- testing_deployment_app/src/main.ts
+++ testing_deployment_app/src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   await app.listen(port,'0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
